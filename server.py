17b69994b7ddf1d386a4e6bbf32c492b38b16682
Hadi Ghazi <hadigghazi@gmail.com>
2024-07-07 05:43:49 +0300

    Adding server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+import yaml
+
+class Server:
+    def __init__(self, config_file):
+        with open(config_file, 'r') as file:
+            self.config = yaml.safe_load(file)
+        self.server_ip = self.config['ServerIPAddress']
+        self.run_localhost = self.config['run_localhost']
+
+    def check_localhost(self):
+        if self.run_localhost:
+            print("Error: run_localhost is set to True")
+
+    def get_server_ip(self):
+        return self.server_ip
+
+if __name__ == "__main__":
+    server = Server('../config.yaml')
+    server.check_localhost()
+    print(server.get_server_ip())
